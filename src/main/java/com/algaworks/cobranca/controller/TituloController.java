fe733a0b15a5d70f5f2ab0d4110ab96da2d7f497
Rodney Pereira <rodneylsp@gmail.com>
2020-05-07 22:25:20 +0100

    Mantendo os dados na tela apos erro de validação.

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,17 +41,17 @@ public class TituloController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ModelAndView salvar(@Validated Titulo titulo, Errors errors){
+    public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes){
         //spring retorna os erros de validacao no objeto errors
 
-        ModelAndView mv = new ModelAndView("CadastroTitulo");
         if(errors.hasErrors()){
-            return mv;
+            return "CadastroTitulo";
         }
 
         repository.save(titulo);
-        mv.addObject("mensagem", "Título salvo com sucesso");
-        return mv;
+        attributes.addFlashAttribute("mensagem", "Título salvo com sucesso");
+
+        return "redirect:/titulos/novo";
     }
 
     @ModelAttribute("todosStatusTitulo")
